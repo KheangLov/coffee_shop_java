3e7a5eb4da78d084c72010cca16d8e78c630677a
lovsokheang <lovsokheang@gmail.com>
2019-08-30 10:45:29 +0700

    Add login form, register form, main form(for login tasks), included db connection

---
+++ src/coffee_shop_java/project/DbConn.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coffee_shop_java.project;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author KHEANG
+ */
+public class DbConn {
+    private static final String serverName = "localhost";
+    private static final String userName = "coffee";
+    private static final String password = "not4you";
+    private static final Integer port = 3306;
+    private static final String dbName = "coffee_shop";
+    
+    public static Connection getConnection(){
+        Connection cnx = null;
+        
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setServerName(serverName);
+        dataSource.setUser(userName);
+        dataSource.setPassword(password);
+        dataSource.setDatabaseName(dbName);
+        dataSource.setPortNumber(port);
+        
+        try {
+            cnx = dataSource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger("Get Connection -> " + anotherformlogin.DbConn.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return cnx;
+    }
+}
---
+++ src/coffee_shop_java/project/User.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coffee_shop_java.project;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author KHEANG
+ */
+public class User extends Action {
+    PreparedStatement stmt = null;
+    private String firstname;
+    private String lastname;
+    private String fullname;
+    private String email;
+    private String password;
+    private String gender;
+    private String status;
+    private int user_type;
+    private String created_date;
+    private String updated_date;
+    
+    public User() {
+        
+    }
+    
+    public String getFirstname() {
+      return firstname;
+    }
+
+    public void setFirstname(String param) {
+      this.firstname = param;
+    }
+    
+    public String getLastname() {
+      return lastname;
+    }
+
+    public void setLastname(String param) {
+      this.lastname = param;
+    }
+    
+    public String getFullname() {
+      return fullname;
+    }
+
+    public void setFullname(String param) {
+      this.fullname = param;
+    }
+    
+    public String getEmail() {
+      return email;
+    }
+
+    public void setEmail(String param) {
+      this.email = param;
+    }
+    
+    public String getPassword() {
+      return password;
+    }
+
+    public void setPassword(String param) {
+      this.password = param;
+    }
+    
+    public String getGender() {
+      return gender;
+    }
+
+    public void setGender(String param) {
+      this.gender = param;
+    }
+    
+    public String getStatus() {
+      return status;
+    }
+    
+    public void setStatus(String param) {
+      this.status = param;
+    }
+    
+    public int getUserType() {
+      return user_type;
+    }
+    
+    public void setUserType(int param) {
+      this.user_type = param;
+    }
+    
+    public String getCreatedDate() {
+      return created_date;
+    }
+    
+    public void setCreatedDate(String param) {
+      this.created_date = param;
+    }
+    
+    public String getUpdatedDate() {
+      return updated_date;
+    }
+    
+    public void setUpdatedDate(String param) {
+      this.updated_date = param;
+    }
+    
+    @Override
+    public void insert() {
+        String sql = "INSERT INTO users(firstname, lastname, fullname, email, password, gender, status, user_type, created_date, updated_date)"
+                + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                //+ "VALUES('" + firstname + "', '" + lastname + "', '" + fullname + "', '" + email + "', '" + password + "', '" + gender + "', '" + status + "', " + user_type + ", '" + created_date + "', '" + updated_date + "')";
+        try {
+            stmt = anotherformlogin.DbConn.getConnection().prepareStatement(sql);
+            stmt.setString(1, firstname);
+            stmt.setString(2, lastname);
+            stmt.setString(3, fullname);
+            stmt.setString(4, email);
+            stmt.setString(5, password);
+            stmt.setString(6, gender);
+            stmt.setString(7, status);
+            stmt.setInt(8, user_type);
+            stmt.setString(9, created_date);
+            stmt.setString(10, updated_date);
+            int i = stmt.executeUpdate();
+            if(i > 0) {
+                JOptionPane.showMessageDialog(null, "Data saved!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Data failed to save!");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
+}
